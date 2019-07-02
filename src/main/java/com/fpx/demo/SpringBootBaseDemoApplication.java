8fa47f7d29fd9bc0fe4301cde53174b6a4249a06
yundiangeqian <1317955420@qq.com>
2019-07-02 15:17:26 +0800

    spring-boot-one-整合druid

---
+++ src/main/java/com/fpx/demo/SpringBootBaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fpx.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBaseDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/fpx/demo/config/DruidConfig.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2016 4PX Information Technology Co.,Ltd. All rights reserved.
+ */
+package com.fpx.demo.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+
+/**
+ * 
+ * @author CaoMian
+ * @date 2019年7月2日
+ */
+@Configuration
+public class DruidConfig {
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @Bean
+    public ServletRegistrationBean druidStatViewServle() {
+        //注册服务
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(
+                new StatViewServlet(), "/druid/*");
+        // 白名单(为空表示,所有的都可以访问,多个IP的时候用逗号隔开)
+        servletRegistrationBean.addInitParameter("allow", "127.0.0.1");
+        // IP黑名单 (存在共同时，deny优先于allow) （黑白名单就是如果是黑名单，那么该ip无法登陆该可视化界面）
+        servletRegistrationBean.addInitParameter("deny", "127.0.0.2");
+        // 设置登录的用户名和密码
+        servletRegistrationBean.addInitParameter("loginUsername", "admin");
+        servletRegistrationBean.addInitParameter("loginPassword", "123456");
+        // 是否能够重置数据.
+        servletRegistrationBean.addInitParameter("resetEnable", "false");
+        return servletRegistrationBean;
+    }
+}
